ad9962a5dca8cf72c59c55aa8708655b60885b2d
Yousef Radwan <128555679+YousefRadwan1@users.noreply.github.com>
2026-06-16 14:28:01 +0300

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.messages import HumanMessage, AIMessage
 from firecrawl import Firecrawl
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 load_dotenv()
 
